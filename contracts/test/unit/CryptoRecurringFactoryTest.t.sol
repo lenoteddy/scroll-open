0fd02191ce72c6bb0306f9a963b4c1ce710b6955
Teddy Leno <lenoteddy@gmail.com>
2025-02-16 01:32:56 +0700

    feat: test script for `CryptoRecurringFactory` 🧪

---
+++ contracts/test/unit/CryptoRecurringFactoryTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {CryptoRecurringFactory} from "src/CryptoRecurringFactory.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+contract CryptoRecurringFactoryTest is Test {
+    CryptoRecurringFactory public cryptoRecurringFactory;
+
+    address public DEPLOYER = makeAddr("deployer");
+    address public USER = makeAddr("user");
+
+    function setUp() external {
+        vm.startBroadcast();
+        cryptoRecurringFactory = new CryptoRecurringFactory();
+        vm.stopBroadcast();
+    }
+
+    function testFactoryCreateWallet() public {
+        cryptoRecurringFactory.createWallet(
+            USER,
+            new address[](0),
+            new address[](0),
+            new address[](0)
+        );
+        assert(cryptoRecurringFactory.getWalletLength() == 1);
+        assert(cryptoRecurringFactory.getWallet(0) != address(0));
+    }
+}
