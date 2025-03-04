1cfbb09358c3ddeef33929076c8c159f3438d211
Teddy Leno <lenoteddy@gmail.com>
2025-03-04 22:58:00 +0700

    feat: footer design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { ConnectKitButton } from "connectkit";
-import Logo from "./assets/logo.png";
 import StringHelper from "./helpers/StringHelper";
+import Logo from "./assets/logo.png";
+import IconTelegram from "./assets/i-telegram.svg";
+import IconX from "./assets/i-x.svg";
 
 function App() {
 	return (
@@ -9,7 +11,7 @@ function App() {
 				<nav className="flex items-center justify-between">
 					<a href="/" className="flex items-center">
 						<img src={Logo} alt="logo" className="w-16 mr-1" />
-						<div>
+						<div className="text-left">
 							<h1 className="pb-1 leading-none text-xl font-semibold">CryptoRecurring</h1>
 							<p className="text-xs">automate your crypto investment</p>
 						</div>
@@ -26,7 +28,26 @@ function App() {
 				</nav>
 			</header>
 			{/* TODO: create body */}
-			{/* TODO: create footer */}
+			<footer className="p-12 rounded-2xl bg-gray-100">
+				<a href="/" className="flex items-center">
+					<img src={Logo} alt="logo" className="w-12 mr-1" />
+					<div className="text-left">
+						<h2 className="pb-1 leading-none text-md font-semibold">CryptoRecurring</h2>
+						<p className="text-xs">automate your crypto investment</p>
+					</div>
+				</a>
+				<p className="my-2 text-justify text-xs text-gray-600">
+					CryptoRecurring.com helps users to automate their crypto investment by setting up recurring investments to buy crypto on a regular schedule.
+				</p>
+				<div className="flex items-center gap-x-4 opacity-50">
+					<a href="https://x.com/cryptorecurring" target="_blank" rel="noreferrer noopener">
+						<img src={IconX} className="h-4" />
+					</a>
+					<a href="https://t.me/+8Pr86bOssPJkMTJl" target="_blank" rel="noreferrer noopener">
+						<img src={IconTelegram} className="h-6" />
+					</a>
+				</div>
+			</footer>
 		</div>
 	);
 }
