56de484991c3aadf812b20f74fd12734646b9bdb
Teddy Leno <lenoteddy@gmail.com>
2025-03-04 12:42:11 +0700

    feat: header design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
+import Logo from "./assets/logo.png";
+
 function App() {
+	const connectWallet = async () => {
+		// TODO: connect wallet functionality
+	};
+
 	return (
 		<div className="container">
-			{/* TODO: create header */}
+			<header>
+				<nav className="flex items-center justify-between">
+					<a href="/" className="flex items-center">
+						<img src={Logo} alt="logo" className="w-16 mr-1" />
+						<div>
+							<h1 className="pb-1 leading-none text-xl font-semibold">CryptoRecurring</h1>
+							<p className="text-xs">automate your crypto investment</p>
+						</div>
+					</a>
+					<button className="btn-connect-wallet" onClick={connectWallet}>
+						Connect Wallet
+					</button>
+				</nav>
+			</header>
 			{/* TODO: create body */}
 			{/* TODO: create footer */}
 		</div>
