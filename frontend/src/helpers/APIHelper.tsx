cc575baab2d5bd55e337da1ae013277f3c3e06e2
Teddy Leno <lenoteddy@gmail.com>
2025-03-07 00:07:13 +0700

    feat: APIHelper setup

---
+++ frontend/src/helpers/APIHelper.tsx
@@ -0,0 +1,28 @@
+// TODO: Implement this function
+const getPlan = () => {
+	return "";
+};
+
+// TODO: Implement this function
+const createPlan = () => {
+	return "";
+};
+
+// TODO: Implement this function
+const modifyPlan = () => {
+	return "";
+};
+
+// TODO: Implement this function
+const getTransactions = () => {
+	return "";
+};
+
+const APIHelper = {
+	getPlan,
+	createPlan,
+	modifyPlan,
+	getTransactions,
+};
+
+export default APIHelper;
