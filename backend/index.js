866a7a5e3630e95e51aad8f7bd5acd7808fb1a89
Teddy Leno <lenoteddy@gmail.com>
2025-03-14 00:39:20 +0700

    feat: plans update api endpoint

--- backend/index.js
+++ backend/index.js
@@ -83,6 +83,30 @@ app.post("/plans", async (req, res) => {
 	}
 });
 
+// TODO: API endpoint for update plans
+app.put("/plans/:id", async (req, res) => {
+	// TODO: do header auth check & use header user address for data queries
+
+	// user inputs
+	const planId = req.params.id;
+	const { address, name, source_token, destination_token, amount, frequency } = req.body;
+	try {
+		// database transaction
+		const connection = await pool.getConnection();
+		const [resultPlan] = await connection.execute(
+			"UPDATE user_plan SET user_plan_name=?, user_plan_source_token=?, user_plan_destination_token=?, user_plan_amount=?, user_plan_frequency=? WHERE user_plan_id=?",
+			[address, name, source_token, destination_token, amount, frequency, planId]
+		);
+		connection.release();
+		console.log("User Operator updated!:", resultOperator);
+		console.log("User Plan updated!:", resultPlan);
+		res.json({ message: "Query successfully executed!" });
+	} catch (err) {
+		console.error("Insert error:", err);
+		res.status(500).send("Internal server error!");
+	}
+});
+
 app.get("/transactions/:planId", async (req, res) => {
 	// TODO: do header auth check & use header user address for data queries
 
